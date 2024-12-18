f7afb9eb13a35a29eb916f0b72c924acb2c1e706
Ilir <ilir.hajdari111@gmail.com>
2024-12-18 18:12:56 -0500

    adding table schemas

---
+++ EmployeeSQL/table_schemas.sql
@@ -0,0 +1,32 @@
+-- Titles Table
+CREATE TABLE titles (
+    title_id VARCHAR PRIMARY KEY,
+    title VARCHAR 
+);
+
+-- Employees Table
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR,
+    birth_date DATE,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex VARCHAR(1),
+    hire_date DATE,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Departments Table
+CREATE TABLE departments (
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR
+);
+
+-- Dept_Manager Table
+CREATE TABLE dept_manager (
+    dept_no VARCHAR,
+    emp_no INT, 
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
